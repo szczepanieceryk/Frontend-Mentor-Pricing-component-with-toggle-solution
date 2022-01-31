5039153fbf151ab07a3c16699d4d89011a626ca4
Eryk Szczepaniec <szczepanieceryk@gmial.com>
2022-01-31 16:28:20 +0100

    add styling for middle component, toggle logic

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const components = document.getElementsByClassName('component');
+// assign elements of a component to variables
 const componentTitle = document.getElementsByClassName('plan');
 const componentSubscription = document.getElementsByClassName('subscription');
 const componentStorage = document.getElementsByClassName('storage');
 const componentUsersAlllowed = document.getElementsByClassName('users-nr');
 const componentTransfer = document.getElementsByClassName('transfer');
 
+const toggleSwitch = document.getElementById('flexSwitchCheckChecked');
+
+// boolena flag for toggle switch
+let isChecked = false;
 
 //  data for prcing component
 const pricingComponentData = [
@@ -34,16 +38,33 @@ const pricingComponentData = [
     },
 ]
 
+// initial component state
 // loop every component and assign values from pricingComponentData Array to right varaibles 
 for (let i = 0; i < pricingComponentData.length; i++) {
     componentTitle[i].innerText = pricingComponentData[i].title
     componentSubscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionMonthly
-    // if (pricingComponentData[i].storage.innerText < 500) {
-    //     componentStorage[i].innerText = pricingComponentData[i].storage + ' GB Storage'
-    // } else {
-    //     componentStorage[i].innerText = pricingComponentData[i].storage + ' TB Storage'
-    // }
+    componentStorage[i].innerText = pricingComponentData[i].storage
+    componentUsersAlllowed[i].innerText = pricingComponentData[i].users + ' Users Allowed'
+    componentTransfer[i].innerText = 'Send up to ' + pricingComponentData[i].transfer + ' GB'
+
+    componentTitle[i].innerText = pricingComponentData[i].title
+    componentSubscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionMonthly
     componentStorage[i].innerText = pricingComponentData[i].storage
     componentUsersAlllowed[i].innerText = pricingComponentData[i].users + ' Users Allowed'
     componentTransfer[i].innerText = 'Send up to ' + pricingComponentData[i].transfer + ' GB'
 }
+
+// change boolean value of isChecked flag
+// change the subscription price
+toggleSwitch.addEventListener('click', () => {
+    isChecked = !isChecked
+    for (let i = 0; i < pricingComponentData.length; i++) {
+        if (isChecked) {
+            componentSubscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionAnnually
+
+        } else {
+            componentSubscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionMonthly
+        }
+    }
+})
+
