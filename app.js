42c87e43e60feef694c3396a185eba0d5f91c3b3
Eryk Szczepaniec <szczepanieceryk@gmial.com>
2022-02-01 13:51:28 +0100

    add READM.md, toggle switch logic

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // assign elements of a component to variables
+const component = document.getElementsByClassName('component')
 const componentTitle = document.getElementsByClassName('plan');
 const componentSubscription = document.getElementsByClassName('subscription');
 const componentStorage = document.getElementsByClassName('storage');
 const componentUsersAlllowed = document.getElementsByClassName('users-nr');
 const componentTransfer = document.getElementsByClassName('transfer');
+const componentButton = document.getElementsByClassName('btn');
 
-const toggleSwitch = document.getElementById('flexSwitchCheckChecked');
+const toggleSwitch = document.getElementById('toggle-switch');
+const switchDiv = document.getElementById('switch');
 
 // boolena flag for toggle switch
 let isChecked = false;
@@ -18,23 +21,26 @@ const pricingComponentData = [
         subscriptionAnnually: 199.99,
         storage: 500,
         users: 2,
-        transfer: 3
+        transfer: 3,
+        btn: 'Learn More'
     },
     {
         title: 'Professional',
         subscriptionMonthly: 24.99,
         subscriptionAnnually: 249.99,
-        storage: '1 TB Storage',
+        storage: 1,
         users: 5,
-        transfer: 10
+        transfer: 10,
+        btn: 'Learn More'
     },
     {
         title: 'Master',
         subscriptionMonthly: 39.99,
         subscriptionAnnually: 399.99,
-        storage: '2 TB Storage',
+        storage: 2,
         users: 10,
-        transfer: 20
+        transfer: 20,
+        btn: 'Learn More'
     },
 ]
 
@@ -43,21 +49,18 @@ const pricingComponentData = [
 for (let i = 0; i < pricingComponentData.length; i++) {
     componentTitle[i].innerText = pricingComponentData[i].title
     componentSubscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionMonthly
-    componentStorage[i].innerText = pricingComponentData[i].storage
+    componentStorage[0].innerText = pricingComponentData[0].storage + ' GB Storage'
+    componentStorage[i].innerText = pricingComponentData[i].storage + ' TB Storage'
     componentUsersAlllowed[i].innerText = pricingComponentData[i].users + ' Users Allowed'
     componentTransfer[i].innerText = 'Send up to ' + pricingComponentData[i].transfer + ' GB'
-
-    componentTitle[i].innerText = pricingComponentData[i].title
-    componentSubscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionMonthly
-    componentStorage[i].innerText = pricingComponentData[i].storage
-    componentUsersAlllowed[i].innerText = pricingComponentData[i].users + ' Users Allowed'
-    componentTransfer[i].innerText = 'Send up to ' + pricingComponentData[i].transfer + ' GB'
+    componentButton[i].innerText = pricingComponentData[i].btn
 }
 
 // change boolean value of isChecked flag
 // change the subscription price
 toggleSwitch.addEventListener('click', () => {
     isChecked = !isChecked
+    switchDiv.classList.toggle('toggle')
     for (let i = 0; i < pricingComponentData.length; i++) {
         if (isChecked) {
             componentSubscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionAnnually
